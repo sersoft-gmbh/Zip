7a95ff719a29ce67c2130fdf49a1995d31d0defc
Xavier Matos <xavi@homerlearning.com>
2019-01-29 13:13:52 -0500

    Added umbrella header to allow for including the framework in playgrounds
    see these links for some details
    https://forums.swift.org/t/recent-swiftpm-features-and-enhancements/13807
    https://forums.swift.org/t/cant-experiment-swiftnio-in-playground/15020/6

---
+++ Zip/minizip/include/Minizip.h
@@ -0,0 +1,15 @@
+//
+//  Minizip.h
+//  Zip
+//
+//  Created by Xavi Matos on 1/29/19.
+//  Copyright © 2019 Roy Marmelstein. All rights reserved.
+//
+
+#ifndef Minizip_h
+#define Minizip_h
+
+#import "unzip.h"
+#import "zip.h"
+
+#endif /* Minizip_h */
